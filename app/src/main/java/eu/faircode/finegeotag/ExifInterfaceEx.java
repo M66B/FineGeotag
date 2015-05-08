23018386c64390148b93d6f24339713d76c3de9e
M66B <marcel@faircode.eu>
2015-05-08 16:47:32 +0200

    Refactoring, tag GPS speed

--- app/src/main/java/eu/faircode/finegeotag/ExifInterfaceEx.java
+++ app/src/main/java/eu/faircode/finegeotag/ExifInterfaceEx.java
@@ -19,38 +19,39 @@ public class ExifInterfaceEx extends ExifInterface {
     }
 
     public void setLocation(Location location) {
+        // Latitude
         double lat = location.getLatitude();
+        this.setAttribute(TAG_GPS_LATITUDE_REF, lat > 0 ? "N" : "S");
+        this.setAttribute(TAG_GPS_LATITUDE, DMS(lat));
+
+        // Longitude
         double lon = location.getLongitude();
+        this.setAttribute(TAG_GPS_LONGITUDE_REF, lon > 0 ? "E" : "W");
+        this.setAttribute(TAG_GPS_LONGITUDE, DMS(lon));
 
-        this.setAttribute(TAG_GPS_LATITUDE_REF, ((lat > 0) ? "N" : "S"));
-        this.setAttribute(TAG_GPS_LONGITUDE_REF, ((lon > 0) ? "E" : "W"));
-
-        int latDeg = (int) Math.abs(lat);
-        int latMin = (int) Math.abs(((lat % 1) * 60));
-        int latSec = (int) Math.abs(((((lat % 1) * 60) % 1) * 60));
-
-        int lonDeg = (int) Math.abs(lon);
-        int lonMin = (int) Math.abs(((lon % 1) * 60));
-        int lonSec = (int) Math.abs(((((lon % 1) * 60) % 1) * 60));
-
-        String latStr = String.format("%d/1,%d/1,%d/1", latDeg, latMin, latSec);
-        String lonStr = String.format("%d/1,%d/1,%d/1", lonDeg, lonMin, lonSec);
-
-        this.setAttribute(TAG_GPS_LATITUDE, latStr);
-        this.setAttribute(TAG_GPS_LONGITUDE, lonStr);
-
+        // Date/time
         Date date = new Date(location.getTime());
+        this.setAttribute(TAG_GPS_DATESTAMP, new SimpleDateFormat("y:M:d").format(date));
+        this.setAttribute(TAG_GPS_TIMESTAMP, new SimpleDateFormat("H:m:s").format(date));
 
-        String dateStamp = new SimpleDateFormat("y:M:d").format(date);
-        String timeStamp = new SimpleDateFormat("H:m:s").format(date);
-
-        this.setAttribute(TAG_GPS_DATESTAMP, dateStamp);
-        this.setAttribute(TAG_GPS_TIMESTAMP, timeStamp);
-
+        // Altitude
         if (location.hasAltitude()) {
             double altitude = location.getAltitude();
-            this.setAttribute(TAG_GPS_ALTITUDE_REF, (altitude > 0 ? "0" : "1"));
+            this.setAttribute(TAG_GPS_ALTITUDE_REF, altitude > 0 ? "0" : "1");
             this.setAttribute(TAG_GPS_ALTITUDE, String.valueOf(Math.abs(altitude)));
         }
+
+        // Speed
+        if (location.hasSpeed()) {
+            this.setAttribute("GPSSpeedRef", "K"); // K/H
+            this.setAttribute("GPSSpeed", String.valueOf(location.getSpeed() * 3600 / 1000));
+        }
+    }
+
+    private static String DMS(double x) {
+        int d = (int) Math.abs(x);
+        int m = (int) Math.abs((x % 1) * 60);
+        int s = (int) Math.abs((((x % 1) * 60) % 1) * 60);
+        return String.format("%d/1,%d/1,%d/1", d, m, s);
     }
 }
